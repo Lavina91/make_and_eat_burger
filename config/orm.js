9f84103fa4eaa2937b8b39c86972d002a557dacf
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-26 16:44:22 -0700

    added basic structure in orm.js

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+// Importing connection to MySQL 
+const connection = require=('./connection.js');
+
+
+// homemade orm object for all SQL statement functions
+
+const orm = {
+    selectAll() {},
+
+    insertOne() {},
+
+    updateOne() {},
+
+};
+
+module.exports = orm;
