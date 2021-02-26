3db8de183d9d619961121309cb0c7971b116b841
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-26 16:27:23 -0700

    added database structure to schema.sql and seeds to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom and Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
