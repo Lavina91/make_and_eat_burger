5bfcc88ec8ac3c4cd8f389e01632cd4070a9a223
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-26 16:37:13 -0700

    created setup code to connect node to MySQL in connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,15 @@
+-- drop any database with the same name 
 DROP DATABASE IF EXISTS burgers_db;
 
+-- create a new DB named burgers_db
 CREATE DATABASE burgers_db;
 
+-- use the burgers_db DB
 USE burgers_db;
+
+
+-- create a table in burgers_db named burgers 
+-- AND holds burger ID, burger name and if it has been eaten or not
 
 CREATE TABLE burgers (
 id INT AUTO_INCREMENT NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
+-- creating a burger with the name mushroom and swiss 
 INSERT INTO burgers (burger_name) VALUES ('Mushroom and Swiss');
+-- creating a burger with the name big kahuna burger
 INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+-- creating a burger with the name veggie burger
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
