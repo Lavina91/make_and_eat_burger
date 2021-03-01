fc787b3a9631edce7e6b3ae201cc2d2f57586f54
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-28 19:17:17 -0700

    added all the routes on burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,6 @@
 // requiring in the express package
 const express = require('express');
+const burger = require('../models/burger.js')
 
 // creating a variable to store the router method
 const router = express.Router();
@@ -7,13 +8,43 @@ const router = express.Router();
 // --------- ROUTES -----------------------------
 
 // homepage route
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+
+    burger.selectAll((data) => {
+        const hbsObject = {
+            burgers: data,
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    })
+
+});
 
 // create burger route
-router.post('/api/burgers', (req, res) => {});
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+        ({id: result.insertId});
+    })
+});
 
 // devoured or not route
-router.put('/api/burgers/:id', (req, res) => {});
+router.put('/api/burgers/:id', (req, res) => {
+
+    const condition = `id = ${req.params.id}`;
+    console.log('condition', condition)
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        (result) => {
+            if (result.changedRows === 0) {
+               return res.status(404).end(); 
+            }
+            res.status(200).end()
+        })
+    });
 
 
 
